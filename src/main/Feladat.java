ffb6b0eb6dad6cf098b3d7ed67723919ce0186f2
Huszár Fruzsina Anna <huszar.fruzsina@akk.lan>
2026-09-10 14:30:20 +0200

    feladat a haromszoges mintajara

---
+++ src/main/Feladat.java
@@ -0,0 +1,54 @@
+package main;
+
+public class Feladat {
+
+    public static int ideiEv = 2026;
+    public static int nyugdijKorhatar = 65;
+    public static void main(String[] args) {
+        feladat();
+    }
+
+    private static void feladat() {
+        String nev = "Fru";
+        int szulEv = 2003;
+        
+        int kor = eletkor(szulEv);
+        int nyugdijEv = nyugdijEve(szulEv);
+
+        ellenorzes(szulEv);
+        
+        koszontes(nev);
+
+        String adat = "Név: %s".formatted(nev);
+        adat += "\nÉletkor (%d-ban): %d év".formatted(ideiEv, kor);
+        adat += "\nNyugdíjazás éve (%d évesen): %d".formatted(nyugdijKorhatar, nyugdijEv);
+        megjelenites(adat);
+    }
+
+    private static void koszontes(String nev) {
+        System.out.println("Szius, " + nev + "!");
+    }
+
+    private static int eletkor(int szulEv) {
+        return ideiEv - szulEv;
+    }
+
+    private static int nyugdijEve(int szulEv) {
+        return szulEv + nyugdijKorhatar;
+    }
+
+    private static void megjelenites(String adat) {
+        System.out.println(adat);
+    }
+
+    private static void ellenorzes(int szulEv) {
+        if (!helyesSzulEvE(szulEv)) {
+            System.err.println("Hiba: A születési év nem lehet a jövőben vagy reálisan túl régen!");
+            throw new IllegalArgumentException("Érvénytelen születési év.");
+        }
+    }
+
+    private static boolean helyesSzulEvE(int szulEv) {
+        return szulEv > 1900 && szulEv <= 2026;
+    }
+}
